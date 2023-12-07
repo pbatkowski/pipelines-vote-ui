5720b118ebefa78a253670cbda20802b54a56d31
null <null>
2023-12-07 14:22:29 +0000

    Added unit test.

---
+++ test_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from app import function1
+
+class TestFunctions(unittest.TestCase):
+
+    def test_function1_returns_none(self):
+        result = function1()
+        self.assertIsNone(result, "function1 should return None")
+
+if __name__ == '__main__':
+    unittest.main()
