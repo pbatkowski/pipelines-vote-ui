99a2a5ee5efc52d5e148968a90b837aca3bdd1b9
null <null>
2023-12-07 16:56:20 +0000

    edit title for santa clause movie.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"The Santa Clause 🎅")
+option_a = os.getenv('OPTION_A', u"The Santa Clause 1 🎅")
 option_b = os.getenv('OPTION_B', u"Die Hard 🔫 🎄")
 hostname = socket.gethostname()
 
