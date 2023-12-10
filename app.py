b2d7abcfbfd98a6bc625cdfbf95c237881f2685b
null <null>
2023-12-10 04:47:36 +0000

    Bugfix test.

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def send_js(path):
 
 # testing function, should return None. Don't use in prod.    
 def function1():
-    return -1
+    return None
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
